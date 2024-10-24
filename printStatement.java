33bee9d9c1ee9f2e6e7f01e2baa54c61bcfc7b7e
PRATHAM PRAKASH KAMBLE <mca.2322@unigoa.ac.in>
2024-10-24 20:50:23 +0530

    Update printStatement.java

--- printStatement.java
+++ printStatement.java
@@ -1,6 +1,6 @@
 public class printStatement {
     public static void main(String[] args) {
         System.out.println("printed the 1 statement, Hello world");
-        System.out.println("printed the 2 statement, Hello world");
+        System.out.println("printed the 2322 statement, Hello world");
     }
 }
